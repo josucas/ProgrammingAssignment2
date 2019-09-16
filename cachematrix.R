95e326fcf3a40aa695412c4c62f7512173eafea0
Josue M Castillo Ruiz <Josue.M.Castillo.Ruiz@py.ey.com>
2019-09-16 00:26:59 -0400

    Programming Assigment2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,35 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+#take the matrix as an input
+makeCacheMatrix <- function(x = matrix()) {
+  
+  inv <- NULL                             ## initialize inv as NULL; will hold value of matrix inverse 
+  set <- function(y) {                   
+    x <<- y                            
+    inv <<- NULL                       
+  }
+  get <- function() x                    ## returns value of the matrix argument
+  
+  Setinverse <- function(inverse) inv <<- inverse  ## assigns value of inv
+  Getinverse <- function() inv                     ## gets the value of inv where called
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ## you need this in order to refer to the functions with the $ operator
 }
 
 
 ## Write a short comment describing this function
+##If the inverse has already been calculated then cacheSolve will retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$Getinverse()
+  if(!is.null(inv)) {
+    message("Cached Data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
